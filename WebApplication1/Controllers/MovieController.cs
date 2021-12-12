9628aed1a0fa12eb5ca142be468bc2216464b11c
EdenGenetTasama <edentasama19@gmail.com>
2021-12-12 16:37:37 +0200

    Add project files.

---
+++ WebApplication1/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "Eden the Queen";
+            return View();
+        }
+
+        public ActionResult MoviesList()
+        {
+
+            string[] arrayOfName = new string[] { "Eden", "Shay", "Daniel", "Oshra", "Bob", "Avner" };
+
+            ViewBag.ListOfMovie = arrayOfName;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowUserName()
+        {
+            ViewBag.UserName = "Eden";
+            return View();
+        }
+
+        public ActionResult ShowUsersInfo()
+        {
+            ViewBag.userObj.firstN = "EDEN GENET";
+            ViewBag.userObj.lastN = "TASAMA";
+            ViewBag.userObj.age = 25;
+
+            return View();
+
+        }
+
+        public ActionResult ShowAllUserNames()
+        {
+            string[] arrayOfNames = new string[] { "Eden", "Shay", "Daniel", "Oshra", "Bob", "Avner" } ;
+            ViewBag.arrayOfNames = arrayOfNames;
+            return View();
+        }
+
+        public ActionResult ShowAllUsersInfo()
+        {
+            return View();
+        }
+    }
+}
